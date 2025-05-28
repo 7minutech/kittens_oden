ede51fc465914bd2a81d18c376c694e9ae9d52cd
7minutech <166534037+7minutech@users.noreply.github.com>
2025-05-28 13:20:22 -0500

    feat: kittens controller destroy action

--- app/controllers/kittens_controller.rb
+++ app/controllers/kittens_controller.rb
@@ -34,6 +34,13 @@ class KittensController < ApplicationController
     @kitten = Kitten.find(params[:id])
   end
 
+  def destroy
+    @kitten = Kitten.find(params[:id])
+    @kitten.destroy
+
+    redirect_to root_path, status: :see_other
+  end
+
   private
   def kitten_params
     params.require(:kitten).permit(:name, :age, :cuteness, :softness)
