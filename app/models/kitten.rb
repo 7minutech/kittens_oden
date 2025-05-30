35217eeb94f95830da8e4a06b69072e33a887e4f
7minutech <166534037+7minutech@users.noreply.github.com>
2025-05-30 09:18:38 -0500

    feat: validation for softness & cuteness for kitten model

--- app/models/kitten.rb
+++ app/models/kitten.rb
@@ -1,4 +1,7 @@
 class Kitten < ApplicationRecord
+  validates :softness, numericality: { greater_than: 0, less_than: 6 }
+  validates :cuteness, numericality: { greater_than: 0, less_than: 6 }
+
   def to_s
     "Name: #{name}\nAge: #{age}\nCuteness #{cuteness}\nSoftness: #{softness}"
   end
